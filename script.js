11b3a0801a73b26321203c19ba7ab71738720923
AdhityaBagusPE <adityaput14@gmail.com>
2021-12-11 15:27:41 +0700

    Input cv

--- script.js
+++ script.js
@@ -1,48 +1,47 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
             $('.navbar').addClass('sticky')
-        }
-        else{
+        } else {
             $('.navbar').removeClass('sticky')
         }
     })
     //TOGGLE BUTTON
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     })
     // Typing animation
-    var typed = new Typed(".typing",{
-        strings:['Web Developer','Backend Developer','Database Administrator','Freelancer','Teacher'],
-        typeSpeed:100,
-        backSpeed:60,
-        loop:true,
+    var typed = new Typed(".typing", {
+        strings: ['Web Developer', 'Backend Developer', 'Database Administrator', 'Freelancer', 'Teacher'],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
     })
-    var typed = new Typed(".typing-2",{
-        strings:['Web Developer','Backend Developer','Database Administrator','Freelancer','Teacher'],
-        typeSpeed:100,
-        backSpeed:60,
-        loop:true,
+    var typed = new Typed(".typing-2", {
+        strings: ['Web Developer', 'Backend Developer', 'Database Administrator', 'Freelancer', 'Teacher'],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
     })
     // owl carousel script
     $('.carousel').owlCarousel({
-        margin:20,
-        loop:true,
-        autoplayTimeOut:2000,
-        autoplayHoverPause:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:false
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
             },
-            600:{
-                items:2,
-                nav:false
+            600: {
+                items: 2,
+                nav: false
             },
-            1000:{
-                items:3,
-                nav:false
+            1000: {
+                items: 3,
+                nav: false
             }
         }
     })
